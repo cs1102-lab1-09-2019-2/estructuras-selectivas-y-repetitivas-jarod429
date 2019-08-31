408ff60b5c2acc0bd00628a98c42b04db9b4bff0
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-30 19:37:03 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,19 @@
-
-
+#include <iostream>
+#include<vector>
+using namespace std;
+vector<int>v;
 int main() {
-
-    return 0;
+  int elementos,mayor=0;
+  cout<<"n:";
+  cin>>elementos;
+  for(int i=0;i<elementos;i++){
+    int elemento;
+    cin>>elemento;
+    v.push_back(elemento);
+  }
+  for(int j=0;j<elementos;++j)
+  if (v[j]>mayor){
+    mayor=v[j];
+  }
+  cout<<"El máximo valor es "<<mayor;
 }
