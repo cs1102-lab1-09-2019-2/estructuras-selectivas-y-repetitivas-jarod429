bf87493d4040d46a20e4d33a8ca553367cf84cce
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-31 16:34:55 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,20 @@
-
-
+#include <iostream>
+#include<math.h>
+using namespace std;
 int main() {
-
-    return 0;
+int numero;
+cin>>numero;
+for (int j=0; j<numero; ++j) {
+      for (int i=0; i<numero ; ++i) {
+     if (j%2==0){cout<<"#";
+       if(i>0&i%2!=0){cout<<"@";}
+       else cout<<"@";
+       }
+     else {cout<<"@";
+     if (i>0&i%2==0){cout<<"#";}
+     else cout<<"#";
+     }
+}
+cout << endl;
+}
 }
