6449923d2501ced59eb2088440764bb43f349e1f
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-30 19:09:11 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,18 @@
-
-
+#include<iostream>
+using namespace std;
 int main() {
+  int n ;
+  cout<<"ingrese un numero ";
+  cin>>n;
+  for (int j=0; j<n; ++j) {
+      for (int i=0; i<n ; ++i) {
+        if(j>0&i>0&i<n-1&j<n-1&i!=j)
+        cout<<" ";
+        else cout<<"*";
+
+      }
+      cout << endl;
+   }
 
     return 0;
 }
