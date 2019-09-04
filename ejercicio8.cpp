1e0ee7f211bfd9bd31debc15ba299473f6b60b5a
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-09-03 21:04:18 -0500

    Update ejercicio8.cpp

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,7 +1,26 @@
-
-
-int main() {
-
-    return 0;
+#include <iostream>
+#include<math.h>
+using namespace std;
+double max(int p,int t);
+double max(int p,int t){
+  int A,B;
+  A=(p*3)/10;
+  B=p-(p/250)*t;
+  if (A>B){return A;}
+  return B ;
+  
 }
-
+int main() {
+int a,b,c,d;
+do{
+cin>>a;
+cin>>b;
+cin>>c;
+cin>>d;}
+while(200>a||b>3500||0>c||d>180);
+double ales=max(a,c);
+double jas=max(b,d);
+if(ales<jas){cout<<"Jasmin ";}
+else if(jas<ales){cout<<"Alessia ";}
+else cout<<"Empate";
+}
