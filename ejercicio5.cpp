7dab0a3b5fa781e402ce300070ab75d4282bd481
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-09-03 19:29:01 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,6 +1,22 @@
-
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main() {
+  double N,xi,sum=0,sumcuadra=0,desvestand;
+  cout<<"N:";
+  cin>>N;
+  int most=1;
+  for(int i=0;i<N;i++){
+    
+    cout<<"elemento"<<most<<":";
+    cin>>xi;
+    sum+=xi;
+    sumcuadra+=pow(xi,2);
+    most++;
 
-    return 0;
+  }
+  desvestand=sqrt((sumcuadra/N)-(pow(sum/N,2)));
+  cout<<"sd:"<<setprecision(3)<<desvestand<<"\n";
 }
